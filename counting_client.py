c49d8ffc33cf495bccb11002d9afc0cd3d65f2e6
root <root@localhost.localdomain>
2016-01-11 17:34:10 -0500

    cleaning up the folder

--- counting_client.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-for n in range(0, 11):
-    s.sendto(str(n).encode('ascii'), ('127.0.0.1', 50007))
-s.sendto(b'QUIT', ('127.0.0.1', 50007))
-s.close()
