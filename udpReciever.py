6da9ab555f2a34dfb9664f95baa3f671072fae10
Robert Shannon <rms5@pdx.edu>
2016-01-19 18:01:16 -0800

    Count packets received

--- udpReciever.py
+++ udpReciever.py
@@ -7,6 +7,9 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind(("10.0.0.9", 6666))
 
+count = 0
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    count += 1
+    print "received message: ", data
+    print "packets received: ", count
